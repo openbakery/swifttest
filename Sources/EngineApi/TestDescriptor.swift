e448bdd162773d4d808a86274251d9b5b6affc2d
David Tanzer <david@davidtanzer.net>
2016-04-19 06:45:03 +0200

    Added some minimal API documentation

--- Sources/EngineApi/TestDescriptor.swift
+++ Sources/EngineApi/TestDescriptor.swift
@@ -1,3 +1,6 @@
+/**
+A test descriptor contains information about a test or a group of tests that is known by a test engine. 
+*/
 public protocol TestDescriptor {
 	var name: String { get }
 	var children: [TestDescriptor] { get }
--- Sources/EngineApi/TestEngine.swift
+++ Sources/EngineApi/TestEngine.swift
@@ -1,3 +1,6 @@
+/**
+A TestEngine is responsible for knowing all tests that have been written using a certain technology and for providing a means to execute them. 
+*/
 public protocol TestEngine {
 	var tests: [TestDescriptor] {get}
 }
